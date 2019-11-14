639db2db5384a19a802448f73d8d9bfd716c034b
FrancoBobadilla <1709673@ucc.edu.ar>
2019-11-14 11:41:56 -0300

    se reorganizo el payroll test y se corrigio un status

--- codeceptjs-rest/Payroll_test.js
+++ codeceptjs-rest/Payroll_test.js
@@ -11,14 +11,11 @@ Scenario('Verificar a successful call a root', async () => {
 	expect(res.data).to.have.nested.property(expectedValues.links.toOrders);
 });
 
+
+// VERIFICAR EMPLOYEES
+
 Scenario('Verificar a successful call a employees', async () => {
 	const res = await I.sendGetRequest('/employees');
-	expect(res.status).to.eql(expectedValues.status.ok);
-        expect(res.data).to.have.nested.property(expectedValues.links.toSelf);
-});
-
-Scenario('Verificar a successful call a orders', async () => {
-	const res = await I.sendGetRequest('/orders');
 	expect(res.status).to.eql(expectedValues.status.ok);
         expect(res.data).to.have.nested.property(expectedValues.links.toSelf);
 });
@@ -47,6 +44,44 @@ Scenario('Verificar segundo employee', async () => {
 	expect(res.data).to.have.nested.property(expectedValues.links.toEmployees);
 });
 
+Scenario('Verificar post a employees', async () => {
+	const res = await I.sendPostRequest('/employees', expectedValues.newEmployee);
+	expect(res.status).to.eql(expectedValues.status.created);
+	expect(res.data.id).to.eql(5);
+	expect(res.data.firstName).to.eql(expectedValues.newEmployee.firstName);
+	expect(res.data.lastName).to.eql(expectedValues.newEmployee.lastName);
+	expect(res.data.role).to.eql(expectedValues.newEmployee.role);
+	expect(res.data.name).to.eql(expectedValues.newEmployee.firstName + " " + expectedValues.newEmployee.lastName);
+        expect(res.data).to.have.nested.property(expectedValues.links.toSelf);
+	expect(res.data).to.have.nested.property(expectedValues.links.toEmployees);
+});
+
+Scenario('Verificar put a employees', async () => {
+	const res = await I.sendPutRequest('/employees/5', expectedValues.modifiedEmployee);
+	expect(res.status).to.eql(expectedValues.status.created);
+	expect(res.data.id).to.eql(5);
+	expect(res.data.firstName).to.eql(expectedValues.modifiedEmployee.firstName);
+	expect(res.data.lastName).to.eql(expectedValues.modifiedEmployee.lastName);
+	expect(res.data.role).to.eql(expectedValues.modifiedEmployee.role);
+	expect(res.data.name).to.eql(expectedValues.modifiedEmployee.firstName + " " + expectedValues.modifiedEmployee.lastName);
+        expect(res.data).to.have.nested.property(expectedValues.links.toSelf);
+	expect(res.data).to.have.nested.property(expectedValues.links.toEmployees);
+});
+
+Scenario('Verificar delete a employees', async () => {
+	const res = await I.sendDeleteRequest('/employees/5');
+	expect(res.status).to.eql(expectedValues.status.notContent);
+});
+
+
+// VERIFICAR ORDER
+
+Scenario('Verificar a successful call a orders', async () => {
+	const res = await I.sendGetRequest('/orders');
+	expect(res.status).to.eql(expectedValues.status.ok);
+        expect(res.data).to.have.nested.property(expectedValues.links.toSelf);
+});
+
 Scenario('Verificar primera order', async () => {
 	const res = await I.sendGetRequest('/orders/3');
 	expect(res.status).to.eql(expectedValues.status.ok);
@@ -71,30 +106,6 @@ Scenario('Verificar segunda order', async () => {
         expect(res.data).to.have.nested.property(expectedValues.links.toComplete);
 });
 
-Scenario('Verificar post a employees', async () => {
-	const res = await I.sendPostRequest('/employees', expectedValues.newEmployee);
-	expect(res.status).to.eql(expectedValues.status.created);
-	expect(res.data.id).to.eql(5);
-	expect(res.data.firstName).to.eql(expectedValues.newEmployee.firstName);
-	expect(res.data.lastName).to.eql(expectedValues.newEmployee.lastName);
-	expect(res.data.role).to.eql(expectedValues.newEmployee.role);
-	expect(res.data.name).to.eql(expectedValues.newEmployee.firstName + " " + expectedValues.newEmployee.lastName);
-        expect(res.data).to.have.nested.property(expectedValues.links.toSelf);
-	expect(res.data).to.have.nested.property(expectedValues.links.toEmployees);
-});
-
-Scenario('Verificar put a employees', async () => {
-	const res = await I.sendPutRequest('/employees/5', expectedValues.modifiedEmployee);
-	expect(res.status).to.eql(expectedValues.status.ok);
-	expect(res.data.id).to.eql(5);
-	expect(res.data.firstName).to.eql(expectedValues.modifiedEmployee.firstName);
-	expect(res.data.lastName).to.eql(expectedValues.modifiedEmployee.lastName);
-	expect(res.data.role).to.eql(expectedValues.modifiedEmployee.role);
-	expect(res.data.name).to.eql(expectedValues.modifiedEmployee.firstName + " " + expectedValues.modifiedEmployee.lastName);
-        expect(res.data).to.have.nested.property(expectedValues.links.toSelf);
-	expect(res.data).to.have.nested.property(expectedValues.links.toEmployees);
-});
-
 Scenario('Verificar post a orders', async () => {
 	const res = await I.sendPostRequest('/orders', expectedValues.newOrder);
 	expect(res.status).to.eql(expectedValues.status.created);
@@ -117,11 +128,6 @@ Scenario('Verificar put a orders', async () => {
 	expect(res.data).to.have.nested.property(expectedValues.links.toOrders);
 	expect(res.data).to.not.have.nested.property(expectedValues.links.toCancel);
         expect(res.data).to.not.have.nested.property(expectedValues.links.toComplete);
-});
-
-Scenario('Verificar delete a employees', async () => {
-	const res = await I.sendDeleteRequest('/employees/5');
-	expect(res.status).to.eql(expectedValues.status.notContent);
 });
 
 Scenario('Verificar delete a orders', async () => {
